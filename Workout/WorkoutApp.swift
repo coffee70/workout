df83e296f1d23a182b5cc8bb1d255f38050dc1d7
Evan <ehaeick2@gmail.com>
2026-04-06 15:35:07 -0400

    Initial Commit

---
+++ Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by Evan Haeick on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
